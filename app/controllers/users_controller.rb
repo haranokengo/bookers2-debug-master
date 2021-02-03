5c171fe69f9748ab50d08ea05867f3cf817bd71e
haranokengo <haranokengo@gmail.com>
2021-02-03 20:12:23 +0000

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user.id)
      end
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -38,6 +38,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
+  post 'follow/:id' => 'relationships#follow', as: 'follow' 
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' 
   devise_for :users
 
   resources :users,only: [:show,:index,:edit,:update]
-
   resources :books do
     resources :book_comments, only: [:create,:destroy]
     resource :favorites, only: [:create,:destroy]
   end
+
 end
