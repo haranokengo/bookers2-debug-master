b92aefb67cb1f510719086d9df1b115a087dd515
haranokengo <haranokengo@gmail.com>
2021-02-03 04:33:48 +0000

    Finish

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,17 @@
-class BookCommentController < ApplicationController
+class BookCommentsController < ApplicationController
 
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.book_comment.new(book_comment_params)
+    comment = BookComment.new(book_comment_params)
+    comment.user_id = current_user.id
     comment.book_id = book.id
     comment.save
-    redirect_to books_path
+     redirect_back(fallback_location: root_path)
   end
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to books_path
+     redirect_back(fallback_location: root_path)
   end
 
   private
